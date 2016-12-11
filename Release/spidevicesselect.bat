e03f61ff52947935843a7d614573f7aec1fd831e
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-11 17:48:55 -0500

    first commit

---
+++ Release/spidevicesselect.bat
@@ -0,0 +1,3 @@
+rem spidevicesselect "" > spidevicesselect.txt
+spidevicesselect "Desktop Microphone (2- HD-5000 " > spidevicesselect.txt
+rem spidevicesselect "Wave (E-MU E-DSP Audio Processo" > spidevicesselect.txt
